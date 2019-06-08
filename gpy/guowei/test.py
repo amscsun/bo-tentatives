f590fb2bab886ac8151d2b9e1803e97e2bb07ce7
amscsun <gwsunofumd@gmail.com>
2019-06-08 14:46:18 -0400

    edit gpy

--- gpy/guowei/test.py
+++
@@ -1,8 +0,0 @@
-# this is for ensuring we are indeed running a local copy of the package
-
-
-class gsTest(s):
-    self.string = s
-
-    def info(self):
-        print(self.string)
---
+++ gpyGradient.py
@@ -0,0 +1,7 @@
+# an implementation for exploiting the gradient from a fitted GP process.
+
+class gpg(l):
+    # l: the lipschitz constant
+
+    def test(self,s):
+        print(s)
