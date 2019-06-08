da5ac6c0b7f556114ceaa6473f9c64bbead05762
amscsun <gwsunofumd@gmail.com>
2019-06-08 14:51:07 -0400

    further

---
+++ gpy/gpyGradient.py
@@ -0,0 +1,8 @@
+# an implementation for exploiting the gradient from a fitted GP process.
+
+class gpg():
+    # l: the lipschitz constant
+    print("callable")
+
+    def info(self,s):
+        print(s)
